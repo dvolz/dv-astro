b3449acc974fcdf91904dcb7c3871f1715ffabc5
dvolz <volzdavid@gmail.com>
2024-12-15 15:16:39 -0800

    Adding slider to cat

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,15 @@ import icon from 'astro-icon'
 import preact from '@astrojs/preact'
 import react from '@astrojs/react'
 
+// Suppress specific warnings
+const originalConsoleWarn = console.warn
+console.warn = (message, ...args) => {
+	if (message.includes('Astro.glob is deprecated')) {
+		return // Ignore this specific warning
+	}
+	originalConsoleWarn(message, ...args) // Allow other warnings
+}
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://dv-astro.netlify.app/',
